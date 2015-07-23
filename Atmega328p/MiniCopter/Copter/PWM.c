2b2a2bb9137dab7986d962f374c0bcbfa29d1631
Ted Varty <evarty@gmail.com>
2015-07-23 19:12:29 -0400

    Set up what I am going to call libraries despite them really just being .c files that are all referenced and compiled and not actually libraries

--- Atmega328p/MiniCopter/Copter/PWM.c
+++
@@ -1,8 +0,0 @@
-#include "PWM.h"
-
-void PWM0Init(char clock 
-
-void OutputPWM0(char Pin, float DutyCycle){
-
-
-}
--- Atmega328p/Voltmeter/ShiftOut.h
+++
@@ -1,4 +0,0 @@
-#ifndef SHIFTOUT_H
-#define SHIFTOUT_H
-void ShiftOut(int SRCLK_Pin, int SER_Pin, int data);
-#endif

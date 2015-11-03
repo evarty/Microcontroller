d30875b4747d538c255482bb0e71bac7e3c987db
Edward <evarty@gmail.com>
2015-11-03 07:34:08 -0500

    Update SPI.h

--- Atmega328p/Libraries/SPI/SPI.h
+++ Atmega328p/Libraries/SPI/SPI.h
@@ -11,6 +11,10 @@ void SPI_MasterInit(void);
 
 uint8_t SPI_MasterTransmitByte(char cData);
 
+void SPI_MasterTransmitArb(uint8_t *DataArray, uint8_t ArrayLength);
+
+void SPI_MasterReceiveArb(uint8_t *DataArray, uint8_t ArrayLength);
+
 void SPI_SlaveInit(void);
 
 char SPI_SlaveRecieve(void);

1bed2bbe91e8686e923436dd06d903c828ff70f6
Edward Varty <evarty@gmail.com>
2015-11-24 17:06:26 -0500

    Fixed light code

--- Attiny45-20PU/LightController/main.c
+++ Attiny45-20PU/LightController/main.c
@@ -17,14 +17,12 @@ int main(void)
 
   TCCR0A |= (1<<WGM01) | (1<<WGM00);//Set into mode 7. PWM with OCR0A as TOP
   TCCR0B |= (1<<WGM02);//Part of previous line, dif register to complete config
-  DDRB |= (1<<1);//Make PB1 a output
+  DDRB |= (1<<1);//Make PB1 an output
+  PORTB &= ~(1<<1);//Force PB1 Low
   TCCR0A |= (1<<COM0B1);//Noninverting PWM mode with output on OC0B (PB1).
   OCR0A = 0x10;//Set OCR0A (TOP) to 16. Makes generated frequency 8Mhz/16=500Khz
 
-  //OCR0B = 5;
-  //TCCR0B |= (1<<CS00);//Activate timer with no prescale
 
-  //OCR0B = 50;
   uint8_t OnState = 0;
   uint8_t OffState = 0;
   uint8_t TurnOnEngaged = 0;
@@ -48,18 +46,27 @@ int main(void)
       OnState = 1;
       OCR0B = 10;
       TCCR0B |= (1<<CS00);
+      _delay_ms(100);
+      TCCR0B &= ~(1<<CS00);
+      _delay_ms(100);
+      PORTB &= ~(1<<1);
     }else if(!TurnOnEngaged && OnState){
       OnState = 0;
+      PORTB &= ~(1<<1);
     }else{;}
 
     if(TurnOffEngaged && !OffState){
       OffState = 1;
       OCR0B = 5;
       TCCR0B |= (1<<CS00);
+      _delay_ms(100);
+      TCCR0B &= ~(1<<CS00);
+      _delay_ms(100);
+      PORTB &= ~(1<<1);
     }else if(!TurnOffEngaged && OffState){
       OffState = 0;
+      PORTB &= ~(1<<1);
     }else{;}
-
 
   }
 }

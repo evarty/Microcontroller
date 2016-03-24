b26a92e422d0ff554efe57da36f260276a20ad30
Edward <evarty@gmail.com>
2016-03-24 11:30:16 -0400

    Update main.c

--- Atmega328p/template/main.c
+++ Atmega328p/template/main.c
@@ -1,4 +1,3 @@
 #include <avr/io.h> //allows more human readable stuff
 #include <avr/interrupt.h>  //allows interrupts
-#include <math.h> //math shit functions and constants
 #include <avr/power.h> //power reduction management

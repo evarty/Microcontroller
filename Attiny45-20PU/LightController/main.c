d06bf418b1cb6ab8cb32913d2ef0b845011c6dd0
Edward Varty <evarty@gmail.com>
2016-04-02 14:36:21 -0400

    added comments

--- Attiny45-20PU/LightController/main.c
+++ Attiny45-20PU/LightController/main.c
@@ -16,13 +16,13 @@ int main(void)
   CLKPR = 0x00;//Set main clock prescale to 1. 8Mhz
 
   TCCR0A |= (1<<WGM01) | (1<<WGM00);//Set into mode 7. PWM with OCR0A as TOP
-  TCCR0B |= (1<<WGM02);//Part of previous line, dif register to complete config
+  TCCR0B |= (1<<WGM02);//Part of previous line, different register to complete config
   DDRB |= (1<<1);//Make PB1 an output
   PORTB &= ~(1<<1);//Force PB1 Low
   TCCR0A |= (1<<COM0B1);//Noninverting PWM mode with output on OC0B (PB1).
   OCR0A = 0x10;//Set OCR0A (TOP) to 16. Makes generated frequency 8Mhz/16=500Khz
 
-
+  //Init state variables
   uint8_t OnState = 0;
   uint8_t OffState = 0;
   uint8_t TurnOnEngaged = 0;
@@ -30,6 +30,7 @@ int main(void)
 
   for(;;){
 
+    //Check whether the lights should be on or off. Because the two input pins are tied together, TurnOnEngaged and TurnOffEngaged will always be opposite. You don't actually need both variables.
     if(TurnOnMask & TurnOnPort){
       TurnOnEngaged = 1;
     }else{
@@ -42,30 +43,34 @@ int main(void)
       TurnOffEngaged = 1;
     }
 
+    //If the lights should be on and they are not, turn them on
     if(TurnOnEngaged && !OnState){
-      OnState = 1;
-      OCR0B = 10;
-      TCCR0B |= (1<<CS00);
+      OnState = 1;//set state variable high to signify that the lights are on
+      OCR0B = 11;//set compare point to 2/3 of OCR0A
+      TCCR0B |= (1<<CS00);//Enable PWM clock and therefore output
       _delay_ms(100);
-      TCCR0B &= ~(1<<CS00);
+      TCCR0B &= ~(1<<CS00);//Disable PWM clock and therefore output
       _delay_ms(100);
-      PORTB &= ~(1<<1);
+      PORTB &= ~(1<<1);//Force PB1 low
+      //If lights are on and signal is low, toggle onstate to off. This works because the two input pins are tied together.
     }else if(!TurnOnEngaged && OnState){
-      OnState = 0;
-      PORTB &= ~(1<<1);
+      OnState = 0;//set state variable
+      PORTB &= ~(1<<1);//force PB1 Low
     }else{;}
 
+    //Iff the lights are on and the turnoff signal is engaged, signal low, turn them off
     if(TurnOffEngaged && !OffState){
-      OffState = 1;
-      OCR0B = 5;
-      TCCR0B |= (1<<CS00);
+      OffState = 1;//set state variable
+      OCR0B = 5;//Set compare point to 1/3 of OCR0A, 1/3 duty cycle
+      TCCR0B |= (1<<CS00);//enable clock and therefore PWM output
       _delay_ms(100);
-      TCCR0B &= ~(1<<CS00);
+      TCCR0B &= ~(1<<CS00);//disable clock and therefor PWM output
       _delay_ms(100);
-      PORTB &= ~(1<<1);
+      PORTB &= ~(1<<1);//Force PB1 low
+      //If lights are off and the signal pin is high, toggle offstate to false. This works because the two input pins are tied together.
     }else if(!TurnOffEngaged && OffState){
-      OffState = 0;
-      PORTB &= ~(1<<1);
+      OffState = 0;//set state variable
+      PORTB &= ~(1<<1);//Force PB1 low
     }else{;}
 
   }

1531d1cfa23a0f3eb9df131e491f9c24595a3041
Edward <evarty@gmail.com>
2016-04-12 16:10:18 -0400

    Update DS1307.h

--- Atmega328p/Libraries/DS1307/DS1307.h
+++ Atmega328p/Libraries/DS1307/DS1307.h
@@ -9,4 +9,7 @@ uint8_t DS1307RegisterR(uint8_t reg);
 void DS1307RegisterWMult(uint8_t *reg, uint8_t RegLength, uint8_t *data, uint8_t dataLength);
 void DS1307RegisterRMult(uint8_t FirstReg, uint8_t NumReg, uint8_t *data);
 
+//Register nicknames
+
+
 #endif

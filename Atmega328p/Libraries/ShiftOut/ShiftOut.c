520f5c3f05c38283f125ce576945bed571992e0c
Edward <evarty@gmail.com>
2016-04-13 13:48:18 -0400

    Update ShiftOut.c

--- Atmega328p/Libraries/ShiftOut/ShiftOut.c
+++ Atmega328p/Libraries/ShiftOut/ShiftOut.c
@@ -5,14 +5,14 @@
 
 
 void ShiftOutByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *DataPORT, uint8_t LatchPin, uint8_t *LatchPORT, uint8_t data){
-  LatchPORT &= ~(1 << LatchPin);  // Set the Latch (register-clock) pin low
+  LatchPORT &= ~(1 << LatchPin);
   
-  for (int i = 0; i < 8; i++){     // Now we are entering the loop to shift out 8+ bits
+  for (int i = 0; i < 8; i++){
 
-    ClockPORT &= ~(1 << CLKPin);                     // Set the serial-clock pin low
-    DataPORT |= (((data & (0x01 << i)) >> i) << DataPin );   // Go through each bit of data and output it
-    ClockPORT |= (1 << CLKPin);                      // Set the serial-clock pin high
-    DataPORT &= ~(((dat a& (0x01 << i)) >> i) << DataPin );  // Set the datapin low again    
+    ClockPORT &= ~(1 << CLKPin);
+    DataPORT |= (((data & (0x01 << i)) >> i) << DataPin );
+    ClockPORT |= (1 << CLKPin); 
+    DataPORT &= ~(((dat a& (0x01 << i)) >> i) << DataPin );
   }
 
   LatchPORT |= (1 << LatchPin); 
@@ -25,10 +25,10 @@ void ShiftOutMultiByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint
   
   for(int i = 0; i < (8 * NumDataByte); i++){
     
-    ClockPORT &= ~(1 << CLKPin);                     // Set the serial-clock pin low
-    DataPORT |= (((data&(0x01<<i))>>i) << DataPin );   // Go through each bit of data and output it
-    ClockPORT |= (1 << CLKPin);                      // Set the serial-clock pin high
-    DataPORT &= ~(((data&(0x01<<i))>>i) << DataPin );  // Set the datapin low again    
+    ClockPORT &= ~(1 << CLKPin);
+    DataPORT |= (((data & (0x01 << i)) >> i) << DataPin );
+    ClockPORT |= (1 << CLKPin);
+    DataPORT &= ~(((data & (0x01 << i)) >> i) << DataPin );
     
   }
   

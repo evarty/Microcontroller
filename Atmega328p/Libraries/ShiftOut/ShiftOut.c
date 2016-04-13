0316a86f5438c8e6feb9a102ffd31634ada8efeb
Edward <evarty@gmail.com>
2016-04-13 13:54:18 -0400

    Update ShiftOut.c

--- Atmega328p/Libraries/ShiftOut/ShiftOut.c
+++ Atmega328p/Libraries/ShiftOut/ShiftOut.c
@@ -2,9 +2,8 @@
 
 #include "ShiftOut.h"
 
-
-
 void ShiftOutByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *DataPORT, uint8_t LatchPin, uint8_t *LatchPORT, uint8_t data){
+  
   LatchPORT &= ~(1 << LatchPin);
   
   for (int i = 0; i < 8; i++){
@@ -13,6 +12,7 @@ void ShiftOutByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *
     DataPORT |= (((data >> i) & 0x01) << DataPin);
     ClockPORT |= (1 << CLKPin); 
     DataPORT &= ~(((data >> i) & 0x01) << DataPin);
+    
   }
 
   LatchPORT |= (1 << LatchPin); 

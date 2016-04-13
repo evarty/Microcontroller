d2d44f624a6b5fe6c67e90c664c5fe976c239615
Edward <evarty@gmail.com>
2016-04-13 14:04:57 -0400

    Update ADC.c

--- Atmega328p/Libraries/ADC/ADC.c
+++ Atmega328p/Libraries/ADC/ADC.c
@@ -9,9 +9,9 @@ void ADCInitilize(void)
   PORTC = 0x00;
   DDRC = 0x00;
 
-  ADMUX |= 0<<REFS0 | 0<<REFS1;
-  ADMUX |= 1<<ADLAR;
-  ADCSRA |= 1<<ADEN | 1<<ADPS2 | 0<<ADPS1 | 0<<ADPS0 | 1<<ADSC;
+  ADMUX |= 0 << REFS0 | 0 << REFS1;
+  ADMUX |= 1 << ADLAR;
+  ADCSRA |= 1 << ADEN | 1 << ADPS2 | 0 << ADPS1 | 0 << ADPS0 | 1 << ADSC;
   loop_until_bit_is_clear(ADCSRA,ADSC);
 }
 
@@ -21,13 +21,13 @@ uint16_t ADCRead(uint8_t ADCPin, uint8_t Resolution)
   r = 0x10;
   r += ADCPin;
   ADMUX = r;
-  ADCSRA |= 1<<ADSC;
+  ADCSRA |= 1 << ADSC;
   loop_until_bit_is_clear(ADCSRA,ADSC);
   uint8_t l = ADCL;
   uint8_t h = ADCH;
 
   if(Resolution){ 
-    return 0x0000| (h<<8)|(l<<0); 
+    return 0x0000 | (h<<8) | (l<<0); 
   }else{
     return l;
   }

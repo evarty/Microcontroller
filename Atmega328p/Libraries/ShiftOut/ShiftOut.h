3374448256deefbff03e14337a7019fb59d04a82
Edward <evarty@gmail.com>
2016-04-13 13:54:11 -0400

    Update ShiftOut.h

--- Atmega328p/Libraries/ShiftOut/ShiftOut.h
+++ Atmega328p/Libraries/ShiftOut/ShiftOut.h
@@ -1,4 +1,5 @@
 #ifndef SHIFTOUT_H
 #define SHIFTOUT_H
-void ShiftOut(int SRCLK_Pin, int SER_Pin, int data);
+void ShiftOutByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *DataPORT, uint8_t LatchPin, uint8_t *LatchPORT, uint8_t data);
+void ShiftOutMultiByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *DataPORT, uint8_t LatchPin, uint8_t *LatchPORT, uint8_t *data, uint8_t NumDataByte);
 #endif

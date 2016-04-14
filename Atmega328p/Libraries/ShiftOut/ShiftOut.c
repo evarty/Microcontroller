b50cda6380818f3af38ea8e738f5e6356a35961d
Edward Varty <evarty@gmail.com>
2016-04-13 22:00:19 -0400

    Altered ShiftOut

--- Atmega328p/Libraries/ShiftOut/ShiftOut.c
+++ Atmega328p/Libraries/ShiftOut/ShiftOut.c
@@ -2,37 +2,37 @@
 
 #include "ShiftOut.h"
 
-void ShiftOutByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *DataPORT, uint8_t LatchPin, uint8_t *LatchPORT, uint8_t data){
+void ShiftOutByte(uint8_t CLKPin, volatile uint8_t *ClockPORT, uint8_t DataPin, volatile uint8_t *DataPORT, uint8_t LatchPin, volatile uint8_t *LatchPORT, uint8_t data){
   
-  LatchPORT &= ~(1 << LatchPin);
+  *LatchPORT &= ~(1 << LatchPin);
   
   for (int i = 0; i < 8; i++){
 
-    ClockPORT &= ~(1 << CLKPin);
-    DataPORT |= (((data >> i) & 0x01) << DataPin);
-    ClockPORT |= (1 << CLKPin); 
-    DataPORT &= ~(((data >> i) & 0x01) << DataPin);
+    *ClockPORT &= ~(1 << CLKPin);
+    *DataPORT |= (((data >> i) & 0x01) << DataPin);
+    *ClockPORT |= (1 << CLKPin); 
+    *DataPORT &= ~(((data >> i) & 0x01) << DataPin);
     
   }
 
-  LatchPORT |= (1 << LatchPin); 
+  *LatchPORT |= (1 << LatchPin); 
 
 }
 
-void ShiftOutMultiByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *DataPORT, uint8_t LatchPin, uint8_t *LatchPORT, uint8_t *data, uint8_t NumDataByte){
+void ShiftOutMultiByte(uint8_t CLKPin, volatile uint8_t *ClockPORT, uint8_t DataPin, volatile uint8_t *DataPORT, uint8_t LatchPin, volatile uint8_t *LatchPORT, uint8_t *data, uint8_t NumDataByte){
 
-  LatchPORT &= ~(1 << LatchPin);
+  *LatchPORT &= ~(1 << LatchPin);
   
   for(int i = 0; i < (8 * NumDataByte); i++){
     
-    ClockPORT &= ~(1 << CLKPin);
-    DataPORT |= (((data[i] >> i ) & 0x01 ) << DataPin );
-    ClockPORT |= (1 << CLKPin);
-    DataPORT &= ~(((data[i] >> i ) & 0x01) << DataPin );
+    *ClockPORT &= ~(1 << CLKPin);
+    *DataPORT |= (((data[i] >> i ) & 0x01 ) << DataPin );
+    *ClockPORT |= (1 << CLKPin);
+    *DataPORT &= ~(((data[i] >> i ) & 0x01) << DataPin );
     
   }
   
-  LatchPORT |= (1 << LatchPin);
+  *LatchPORT |= (1 << LatchPin);
 
 }
 
--- Atmega328p/Libraries/ShiftOut/ShiftOut.h
+++ Atmega328p/Libraries/ShiftOut/ShiftOut.h
@@ -1,5 +1,5 @@
 #ifndef SHIFTOUT_H
 #define SHIFTOUT_H
-void ShiftOutByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *DataPORT, uint8_t LatchPin, uint8_t *LatchPORT, uint8_t data);
-void ShiftOutMultiByte(uint8_t CLKPin, uint8_t *ClockPORT, uint8_t DataPin, uint8_t *DataPORT, uint8_t LatchPin, uint8_t *LatchPORT, uint8_t *data, uint8_t NumDataByte);
+void ShiftOutByte(uint8_t CLKPin, volatile uint8_t *ClockPORT, uint8_t DataPin, volatile uint8_t *DataPORT, uint8_t LatchPin, volatile uint8_t *LatchPORT, uint8_t data);
+void ShiftOutMultiByte(uint8_t CLKPin, volatile uint8_t *ClockPORT, uint8_t DataPin, volatile uint8_t *DataPORT, uint8_t LatchPin, volatile uint8_t *LatchPORT, uint8_t *data, uint8_t NumDataByte);
 #endif

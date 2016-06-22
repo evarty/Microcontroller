05d4b64d0ae8fa7e95661d6c6f3698a77b28a2c5
Edward Varty <evarty@gmail.com>
2016-06-21 21:08:36 -0400

    began spider

---
+++ Atmega328p/Spider/main.c
@@ -0,0 +1,11 @@
+#include <avr/io.h> //allows more human readable stuff
+#include <avr/interrupt.h>  //allows interrupts
+#include <avr/power.h> //power reduction management
+
+#include "IIC.h"
+
+while(1){
+
+
+  
+}
